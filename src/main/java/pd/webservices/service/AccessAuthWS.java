d2a9a2f8dd5cbe49f4de1790e7b7ee9d0d731cee
francisco oliveira <f0l1v31r4@hotmail.com>
2015-04-08 00:58:54 -0300

    Habilitando o WebMethod getListObject() do WebServices

--- src/main/java/pd/webservices/service/AccessAuthWS.java
+++ src/main/java/pd/webservices/service/AccessAuthWS.java
@@ -1,5 +1,6 @@
 package pd.webservices.service;
 
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -15,7 +16,15 @@ public class AccessAuthWS implements IAccessAuth{
     
     @Override
     @WebMethod(operationName = "login")
-    public String login(@WebParam(name = "name") String name,@WebParam(name = "passwd") String passwd) {
+    public String login(@WebParam(name = "name") String name,@WebParam(name = "passwd") String passwd) throws Exception {
         return String.format("login of %s %s",name,passwd);
     }
+
+  @Override
+  @WebMethod(operationName = "getListObject")
+  public List<String> getListObject(@WebParam(name = "session")  String session) throws Exception
+  {
+    throw new UnsupportedOperationException("Not supported yet."); 
+  }
+    
 }
