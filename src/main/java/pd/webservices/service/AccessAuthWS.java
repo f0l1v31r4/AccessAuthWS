bd6b15f688700bcac046e472e7cc2f34150bf3c8
francisco oliveira <f0l1v31r4@hotmail.com>
2015-04-08 01:25:24 -0300

    Criando o WebMethod createUser()

--- src/main/java/pd/webservices/service/AccessAuthWS.java
+++ src/main/java/pd/webservices/service/AccessAuthWS.java
@@ -1,30 +1,42 @@
 package pd.webservices.service;
 
 import java.util.List;
+import java.util.Map;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import pd.webservices.model.AbstractShape;
 import pd.webservices.model.IAccessAuth;
+import pd.webservices.model.IPermission;
 
 /**
  *
  * @author foliveira
  */
 @WebService(serviceName = "AccessAuthWS")
-public class AccessAuthWS implements IAccessAuth{
+public class AccessAuthWS implements IAccessAuth
+{
 
-    
-    @Override
-    @WebMethod(operationName = "login")
-    public String login(@WebParam(name = "name") String name,@WebParam(name = "passwd") String passwd) throws Exception {
-        return String.format("login of %s %s",name,passwd);
-    }
+  
+  @Override
+  @WebMethod(operationName = "login")
+  public String login(@WebParam(name = "name") String name, @WebParam(name = "passwd") String passwd) throws Exception
+  {
+    return String.format("login of %s %s", name, passwd);
+  }
 
   @Override
   @WebMethod(operationName = "getListObject")
-  public List<String> getListObject(@WebParam(name = "session")  String session) throws Exception
+  public List<String> getListObject(@WebParam(name = "session") String session) throws Exception
   {
-    throw new UnsupportedOperationException("Not supported yet."); 
+    throw new UnsupportedOperationException("Not supported yet.");
   }
-    
+
+  @Override
+  @WebMethod(operationName = "createUser")
+  public boolean createUser(@WebParam(name = "name") String name, @WebParam(name = "passwd") String passwd) throws Exception
+  {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
 }
