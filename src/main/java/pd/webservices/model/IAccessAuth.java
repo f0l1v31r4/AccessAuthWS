b4c7c04f82631bd2ff5611af1a89cab561d1009d
francisco oliveira <f0l1v31r4@hotmail.com>
2015-04-08 00:53:36 -0300

    Habilitando o metodo getListObject() do WebServices

--- src/main/java/pd/webservices/model/IAccessAuth.java
+++ src/main/java/pd/webservices/model/IAccessAuth.java
@@ -1,18 +1,16 @@
 package pd.webservices.model;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-//import java.util.List;
+import java.util.List;
 
 /**
  *
  * @author foliveira
  */
-public interface IAccessAuth extends Remote
+public interface IAccessAuth
 {
-  public String login(String name, String passwd) throws RemoteException;
+  public String login(String name, String passwd) throws Exception;
 //  public boolean createUser(String name, String passwd) throws RemoteException;
-//  public List<String> getListObject(String session) throws RemoteException;
+  public List<String> getListObject(String session) throws Exception;
 //  public AbstractShape getObject(String session, String id) throws RemoteException;
 //  public boolean writeObject(String session,AbstractShape shape) throws RemoteException;
 //  public boolean removeObject(String session,String id) throws RemoteException;
