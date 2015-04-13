1aa52cea5bdd5526f3bbd6675115092204f78a66
francisco oliveira <f0l1v31r4@hotmail.com>
2015-04-13 17:28:10 -0300

    Habilitando os métodos writeObject e removeObject

--- src/main/java/common/IAccessAuth.java
+++ src/main/java/common/IAccessAuth.java
@@ -12,7 +12,7 @@ public interface IAccessAuth
   public boolean createUser(String name, String passwd) throws Exception;
   public List<String> getListObject(String session) throws Exception;
   public AbstractShape getObject(String session, String id) throws Exception;
-//  public boolean writeObject(String session,AbstractShape shape) throws Exception;
-//  public boolean removeObject(String session,String id) throws Exception;
+  public boolean writeObject(String session,AbstractShape shape) throws Exception;
+  public boolean removeObject(String session,String id) throws Exception;
   
 }
